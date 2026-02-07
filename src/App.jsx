f0cc7803384ac686a20d27e140e8e27a49a362e1
AlejaR522 <aleja.dev2004@gmail.com>
2026-02-06 21:28:28 -0500

    refactor: modularize game logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import confetti from 'canvas-confetti'
 
-const TURNS = {
-    X: 'x',
-    O: 'o'
-}
 
-
-const Square = ({ children, isSelected, updateBoard, index }) => { 
-    const className = `square ${isSelected ? 'is-selected' : ''}`
-
-    const handleClick = () => {
-        updateBoard(index)
-    }
-    return (
-        <div onClick={handleClick} className={className}>
-            {children}
-        </div> 
-    ) 
-}
-
-const WINNER_COMBOS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+import { Square } from './assets/components/Square'
+import { TURNS } from './assets/constans'
+import { checkWinner, checkEndGame } from './logic/board'
+import { WinnerModal } from './assets/components/WinnerModal'
 
 function App() {
     const [board, setBoard]=  useState( Array(9).fill(null)
@@ -42,36 +18,12 @@ function App() {
 
     const [winner, setWinner] = useState(null)
 
-    const checkWinner = (boardToCheck) => {
-        // revisamos todas las combinaciones ganadoras
-        // para ver si x u o ganó
-        for (const combo of WINNER_COMBOS) {
-            const [a, b, c] = combo
-            if(
-                boardToCheck[a] &&
-                boardToCheck[a] === boardToCheck[b] &&
-                boardToCheck[a] === boardToCheck[c]
 
-            ) {
-                return boardToCheck[a]
-            }
-        }
-        // si no hay ganador
-        return null
-    }
     const resetGame = () => {
         setBoard(Array(9).fill(null))
         setTurn(TURNS.X)
         setWinner(null)
         // Depende mucho de lo que busques hacer, de la logica del funcionamiento del juego
-    }
-
-    const checkEndGame = (newBoard) => {
-        //revisamos si hay empate
-        // si no hay mas espacios vacios
-        //en el tablero
-
-        return newBoard.every((square) => square !== null)
     }
 
     const updateBoard = (index) => {
@@ -126,29 +78,8 @@ function App() {
                 {TURNS.O}
             </Square>
         </section>
-        {
-            winner !== null && (
-                <section>
-                    <div className="winner">
-                        <h2>
-                            {
-                            winner == false
-                            ? 'Empate'
-                            : 'Ganó'
-                            }
 
-                        </h2> 
-                        <header className="win">
-                            {winner && <Square>{winner}</Square>}
-                        </header>    
-
-                        <footer>
-                            <button onClick={resetGame}>Empezar de nuevo</button>
-                        </footer>               
-                    </div>
-                </section>
-            )
-        }
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
     )
 }
